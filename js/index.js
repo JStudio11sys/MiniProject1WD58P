396890f662e717395519a93bb4783b2f498c75dc
JStudio11sys <126428980+JStudio11sys@users.noreply.github.com>
2023-03-17 13:36:11 +0800

    updated animation
    updated cloud and student animation

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ function scrollFunction() {
         window.addEventListener('scroll', function(){
             let value = window.scrollY;
             sky.style.top = value * 0.25 + 'px';
-            clouds.style.top = value * 1.05 + 'px';
+            clouds.style.left = value * 1.05 + 'px';
             facade.style.top = value * 0.5 + 'px';
             students.style.left = value * -3 + 'px';
             text.style.marginRight = value * 3.6 + 'px';
